2c1e3484ca69cacee497b0167bcd30bb408fbbce
Justin Carter <justincarterdev@gmail.com>
2026-08-22 18:58:03 -0500

    Configure kitty

--- home/.config/fish/conf.d/colored_man.fish
+++ home/.config/fish/conf.d/colored_man.fish
@@ -4,9 +4,9 @@ functions -c man __man
 
 function man --wraps man
     set -x LESS_TERMCAP_md (set_color -o blue) # Start bold
-    set -x LESS_TERMCAP_me (set_color normal) # End bold
+    set -x LESS_TERMCAP_me (set_color normal)  # End bold
     set -x LESS_TERMCAP_us (set_color -u cyan) # Start underline
-    set -x LESS_TERMCAP_ue (set_color normal) # End underline
+    set -x LESS_TERMCAP_ue (set_color normal)  # End underline
 
     __man $argv
 end
